dca4ebd418c357a4e8c33abf64e7e96f8e7ed6d4
Tanisha <tanisha031199@gmail.com>
2020-03-21 01:59:21 +0530

    added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `a11y Friendly`,
+    description: `Kick off your next, accessible project with this blog`,
+    author: `@tanisha031199`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,10 +10,17 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`
+      }
+    },
+    `gatsby-transformer-mdx`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -24,11 +31,19 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js")
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+// import Image from "../components/image";
+import { useStaticQuery, graphql } from "gatsby";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import SEO from "../components/seo";
 
-export default IndexPage
+const IndexPage = () => {
+  const posts = useStaticQuery(
+    graphql`
+      query {
+        allMdx {
+          nodes {
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    `
+  );
+  console.log(posts);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        {/* <Image /> */}
+      </div>
+      {posts.allMdx.nodes.map(p => (
+        <pre>{JSON.stringify(p, null, 2)}</pre>
+      ))}
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
+
+export default IndexPage;
