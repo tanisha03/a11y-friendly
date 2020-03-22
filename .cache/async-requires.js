bdb23d76e7f8cb063208bc9bbd6ee03c4d22fab1
Tanisha <tanisha031199@gmail.com>
2020-03-23 01:13:50 +0530

    added nav

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,9 +5,9 @@ exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-posts-contact-mdx": () => import("./../src/posts/contact.mdx" /* webpackChunkName: "component---src-posts-contact-mdx" */),
   "component---src-posts-contact-2-mdx": () => import("./../src/posts/contact2.mdx" /* webpackChunkName: "component---src-posts-contact-2-mdx" */),
+  "component---src-posts-nav-mdx": () => import("./../src/posts/nav.mdx" /* webpackChunkName: "component---src-posts-nav-mdx" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("./../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-posts-nav-mdx": () => import("./../src/posts/nav.mdx" /* webpackChunkName: "component---src-posts-nav-mdx" */)
+  "component---src-pages-page-2-js": () => import("./../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
 }
 
