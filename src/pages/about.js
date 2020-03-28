363c7c0d47030673fce8296a8481fa940b75ca2c
Tanisha <tanisha031199@gmail.com>
2020-03-29 01:40:45 +0530

    added

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,14 +8,18 @@ const About = () => {
       <p>
         Hey there, fellow a11y enthusiast !!! I am{" "}
         <a href="https://twitter.com/tanishaaa03">Tanisha Sabherwal</a>. It is
-        great to have you onboard in this journey. I hope you're also trying to
-        make web a better place for all by making it super accessible. Here is
-        my little effort to do the same.
+        great to have you onboard in this journey. If you're wondering, what is
+        a11y, go check it out{" "}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/Accessibility">
+          here
+        </a>
+        . I hope you're also trying to make web a better place for all by making
+        it super accessible. Here is my little effort to do the same.
       </p>
       <br />
       <p>
         Accessibility is an often ignored topic when it comes to development. We
-        fail to realize its impact but it and how great an affects it causes to
+        fail to realize its impact but it and how great an affect it causes to
         any business out there. It is not just limited to people with physical
         disabilities rather it also incorporates{" "}
         <a href="https://userway.org/blog/how-situational-disabilities-impact-us-all">
