90db63a3c0188bb7b0748daae25107d1841ddf7d
Tanisha <tanisha031199@gmail.com>
2020-03-30 01:28:57 +0530

    added

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => {
       <p>
         Hey there, fellow a11y enthusiast !!! I am{" "}
         <a href="https://twitter.com/tanishaaa03">Tanisha Sabherwal</a>. It is
-        great to have you onboard in this journey. If you're wondering, what is
+        great to have you onboard in this journey. If you're not sure, what is
         a11y, go check it out{" "}
         <a href="https://developer.mozilla.org/en-US/docs/Web/Accessibility">
           here
@@ -18,22 +18,22 @@ const About = () => {
       </p>
       <br />
       <p>
-        Accessibility is an often ignored topic when comes to development. We
-        fail to realize its impact but it and how great an affect it causes to
-        any business out there. It is not just limited to people with physical
+        Accessibility is an often ignored topic when it comes to development. We
+        fail to realize its impact and how great an affect it causes to any
+        business out there. It is not just limited to people with physical
         disabilities rather it also incorporates{" "}
         <a href="https://userway.org/blog/how-situational-disabilities-impact-us-all">
           situational impairment
         </a>
-        . Hence, it is extremely essential to us to develop things that can
-        seamlessly interact with all out users. Remember, ther web itself is
+        . Hence, it is extremely essential for us to develop things that can
+        seamlessly interact with all our users. Remember, the web itself is
         accessible, it is our way of doing certain things that breaks this
         accessibility.
       </p>
       <br />
       <h3>
         The next time you think of writing a cleaner code, make it sure to think
-        of writing an accessible code as well.
+        of writing an "accessible" code as well.
       </h3>
     </Layout>
   );
