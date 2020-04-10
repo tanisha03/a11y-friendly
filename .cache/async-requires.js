ee9a00dd707ea58345017017990b8a85de432236
Tanisha <tanisha031199@gmail.com>
2020-04-10 23:07:38 +0530

    added changes

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,13 +3,13 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-posts-10-common-a-11-y-mistakes-mdx": () => import("./../src/posts/10-common-a11y-mistakes.mdx" /* webpackChunkName: "component---src-posts-10-common-a-11-y-mistakes-mdx" */),
   "component---src-posts-accessible-dropdown-navigation-mdx": () => import("./../src/posts/accessible-dropdown-navigation.mdx" /* webpackChunkName: "component---src-posts-accessible-dropdown-navigation-mdx" */),
   "component---src-posts-accessible-form-mdx": () => import("./../src/posts/accessible-form.mdx" /* webpackChunkName: "component---src-posts-accessible-form-mdx" */),
   "component---src-posts-visually-hidden-mdx": () => import("./../src/posts/visually-hidden.mdx" /* webpackChunkName: "component---src-posts-visually-hidden-mdx" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("./../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-contact-js": () => import("./../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-posts-10-common-a-11-y-mistakes-mdx": () => import("./../src/posts/10-common-a11y-mistakes.mdx" /* webpackChunkName: "component---src-posts-10-common-a-11-y-mistakes-mdx" */)
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,13 +6,13 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/.cache/dev-404-page.js"))),
+  "component---src-posts-10-common-a-11-y-mistakes-mdx": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/posts/10-common-a11y-mistakes.mdx"))),
   "component---src-posts-accessible-dropdown-navigation-mdx": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/posts/accessible-dropdown-navigation.mdx"))),
   "component---src-posts-accessible-form-mdx": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/posts/accessible-form.mdx"))),
   "component---src-posts-visually-hidden-mdx": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/posts/visually-hidden.mdx"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/pages/contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/pages/index.js"))),
-  "component---src-posts-10-common-a-11-y-mistakes-mdx": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/posts/10-common-a11y-mistakes.mdx")))
+  "component---src-pages-index-js": hot(preferDefault(require("/home/tanisha/Documents/a11y-friendly/src/pages/index.js")))
 }
 
