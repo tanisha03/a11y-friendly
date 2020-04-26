273450b9ef4c88ee6162c35e8df9ba1fafd8e745
Tanisha <tanisha031199@gmail.com>
2020-04-27 03:12:41 +0530

    changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer>© {new Date().getFullYear()}, Built with &hearts;</footer>
+  <footer>© {new Date().getFullYear()}, Built with &hearts; by <a href="https://twitter.com/tanishaaa03" target="_blank" style={{color:"white", textDecoration:"underline"}}>@tanishaaa03</a></footer>
 );
 
 export default Footer;
