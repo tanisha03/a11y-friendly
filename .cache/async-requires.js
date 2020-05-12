d395fa182618b9eadf52a43c0006d0acd971a169
Tanisha <tanisha031199@gmail.com>
2020-05-12 17:48:20 +0530

    miinor changes:

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,10 +3,10 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-posts-accessible-form-mdx": () => import("./../src/posts/accessible-form.mdx" /* webpackChunkName: "component---src-posts-accessible-form-mdx" */),
   "component---src-posts-10-common-a-11-y-mistakes-mdx": () => import("./../src/posts/10-common-a11y-mistakes.mdx" /* webpackChunkName: "component---src-posts-10-common-a-11-y-mistakes-mdx" */),
-  "component---src-posts-accessible-neumorphism-mdx": () => import("./../src/posts/accessible-neumorphism.mdx" /* webpackChunkName: "component---src-posts-accessible-neumorphism-mdx" */),
   "component---src-posts-accessible-dropdown-navigation-mdx": () => import("./../src/posts/accessible-dropdown-navigation.mdx" /* webpackChunkName: "component---src-posts-accessible-dropdown-navigation-mdx" */),
+  "component---src-posts-accessible-form-mdx": () => import("./../src/posts/accessible-form.mdx" /* webpackChunkName: "component---src-posts-accessible-form-mdx" */),
+  "component---src-posts-accessible-neumorphism-mdx": () => import("./../src/posts/accessible-neumorphism.mdx" /* webpackChunkName: "component---src-posts-accessible-neumorphism-mdx" */),
   "component---src-posts-neumorphism-mdx": () => import("./../src/posts/neumorphism.mdx" /* webpackChunkName: "component---src-posts-neumorphism-mdx" */),
   "component---src-posts-visually-hidden-mdx": () => import("./../src/posts/visually-hidden.mdx" /* webpackChunkName: "component---src-posts-visually-hidden-mdx" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
